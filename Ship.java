166dc9eebfaf4e707c3f07a715488628227ab827
Kristen <kristenjwoods@gmail.com>
2015-12-07 16:33:13 -0600

    Create Ship.java

---
+++ Ship.java
@@ -0,0 +1,34 @@
+public class Ship {
+    private int length = 1;
+    private boolean[] wasHit;
+    private boolean isVertical;
+    private Cell[][]cellsOccupied;
+
+    public int getLength() {
+       return length; 
+    }
+    
+    public boolean getWasHit() {
+        
+    }
+    public void setWasHit(boolean wasHit) {
+        
+    }
+    
+    public Ship(int length, int type, boolean isVertical) {
+        
+    }
+    
+    public boolean getIsVertical() {
+        
+    }
+    
+    public void setIsVertical(boolean isVertical) {
+        
+    }
+    
+    public boolean isSunk() {
+        
+    }
+    
+}
