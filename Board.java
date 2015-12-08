3b32e90be2ae2ec74c2b0408accab9d6efa7f5d0
KirbySonic <katiewho64@gmail.com>
2015-12-08 12:14:56 -0600

    Board Class
    I think I figured out how to upload my file. (Go me!?)

---
+++ Board.java
@@ -0,0 +1,43 @@
+public class Board {
+    private int rows = 0;
+    private int columns = 0;
+    private Cell[][] cells;
+    private Ship[] ships;
+    private int numships = 0;
+    
+    public int getRows() {
+        return 0;
+    }
+    
+    public int getColumns() {
+        return 0;
+    }
+    
+    public Cell[][]  getCells(int i, int j) {
+        return null;
+    }
+   
+    public Ship[] getShips() {
+        return null;
+    }
+    
+    public void addShip(Ship[] ships) {
+        
+    }
+    
+    public int getNumShips() {
+        return 0;
+    }
+    
+    public void setUp() {
+        
+    }
+    
+    public void display() {
+        
+    }
+    
+    public void placeShips() {
+        
+    }
+}
