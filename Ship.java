5a744066cfd04ccc69c774d9973e0acf878571e1
mrsbrock <docs.jennifbrock@gmail.com>
2015-12-09 22:59:11 -0600

    Moved Kristen's Changes Over

--- Ship.java
+++ Ship.java
@@ -1,33 +1,41 @@
 public class Ship {
     private int length = 1;
     private boolean[] wasHit;
-    private boolean isVertical;
-    private Cell[][]cellsOccupied;
-
-    public Ship(int length, int type, boolean isVertical) {
+    private boolean isVertical;  
+    private Cell[]cellsOccupied;  
+    
+    public Ship(int length, int type, boolean isVertical) {  
+         
+    }  
+      
+     public int getLength() {  
+         return length;   
+    }  
         
+    public boolean getWasHit() {  
+          
+        return false;  
+    }  
+    public void setWasHit(boolean wasHit) {  
+        this.wasHit = wasHit;  
     }
     
-    public int getLength() {
-       return length; 
-    }
-    
-    public boolean getWasHit() {
-        return false;
-    }
-    public void setWasHit(boolean wasHit) {
+    public int getCellsOccupied[] {
+        return cellsOccupied;
+    }    
         
+     public boolean getIsVertical() {  
+          
+        return isVertical;  
     }
-    
-    public boolean getIsVertical() {
-        return isVertical;
-    }
-    
+      
     public void setIsVertical(boolean isVertical) {
+        this.isVertical =isVertical;
+    }  
         
-    }
-    
-    public boolean isSunk() {
-        return false;
-    }
-}
+    public boolean isSunk() {  
+          
+        return false;  
+      }  
+
+}  
