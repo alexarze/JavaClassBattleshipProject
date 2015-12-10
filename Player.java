995951a1f52156ec5b935a03453a1b195195b2ff
mrsbrock <docs.jennifbrock@gmail.com>
2015-12-10 15:19:04 -0600

    Moved Ben's Player Updates Into GitHub

--- Player.java
+++ Player.java
@@ -1,6 +1,5 @@
 public class Player {
 
-    //The default name should not be Peter, it should be null or ""
     private String name = null;
     private int numOfGuesses = 0;
     private int numOfHits = 0;
@@ -17,14 +16,16 @@ public class Player {
     public int getNumOfGuesses() {
         return numOfGuesses;
     }
-    public void addHit(int h){
+    public void addHit(){
+        this.numOfHits+=1;
     }
-    public void addGuess(int g){
+    public void addGuess(){
+        this.numOfGuesses+=1;
     }
     public double getAccuracy(){
         //For now, we are going to return 0 because we haven't given you the formula you need to calculate the accuracy.
         //And there is no variable called Accuracy, so that was your compile error.
         //return Accuracy;
-        return 0;
+        return (numOfHits/numOfGuesses);
     }
 }
