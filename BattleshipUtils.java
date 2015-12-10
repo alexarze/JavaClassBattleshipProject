f59c9a5116878875d84fba0ab6e83b7b41efa5ca
mrsbrock <docs.jennifbrock@gmail.com>
2015-12-10 12:25:58 -0600

    Updated BattleshipUtils
    On BattleshipUtils:
    1.  Added comments
    2.  Changed "size" to "length" and changed ShipTypes to ShipSpecs
    3.  Added BoardSpecs enum to specify the size of the board and the
    appropriate number of ships for that board size.
    
    TestBattleshipUtils:
    1.  changed test methods to print methods since they don't actually test
    anything, they just show how to use the enum
    2.  Added testShipSpecsLong and testShipSpecsShort method testing the
    ShipSpecs using the long and short form of the switch statement.

--- BattleshipUtils.java
+++ BattleshipUtils.java
@@ -8,16 +8,40 @@ public class BattleshipUtils {
     public static char DISPLAY_MISS = '_';
     public static char DISPLAY_UNGUESSED = 'O';
     
-    public enum ShipTypes {
+    /*
+     * This is an enumerator obect that defines the types of valid ships
+     * and their corresponding sizes.
+     */
+    public enum ShipSpecs {
         AIRCRAFT_CARRIER (5),
         BATTLESHIP (4),
         SUBMARINE (3),
         DESTROYER (3),
         PATROL_BOAT (2);
         
-        public final int size;
-        ShipTypes(int size) {
-            this.size = size;
+        public final int length;
+        ShipSpecs(int length) {
+            this.length = length;
+        }
+    }
+    
+    /*
+     * This is an enumerator object that defines the specs for three
+     * different board sizes - defining the rows, columns and the appropriate
+     * number of ships for that board size.
+     */
+    public enum BoardSpecs {
+        SMALL_BOARD (5, 5, 3), //Any 3 random ships
+        MEDIUM_BOARD (10, 10, 5),  //One of each ship
+        LARGE_BOARD (40, 40, 10);  //Two of each ship
+        
+        public final int rows;
+        public final int cols;
+        public final int numShips;
+        BoardSpecs(int rows, int cols, int numShips) {
+            this.rows = rows;
+            this.cols = cols;
+            this.numShips = numShips;
         }
     }
 }
--- TestBattleshipUtils.java
+++ TestBattleshipUtils.java
@@ -1,10 +1,65 @@
 public class TestBattleshipUtils {
-    public static void testShipTypes() {
-        for (BattleshipUtils.ShipTypes type : BattleshipUtils.ShipTypes.values()) {
-            System.out.println(type + " has a size of " + type.size + ".");
+    public static void printShipSpecs() {
+        for (BattleshipUtils.ShipSpecs type : BattleshipUtils.ShipSpecs.values()) {
+            System.out.println(type + " has a length of " + type.length + ".");
         }        
     }
+    
+    public static void testShipSpecsLong() {
+        boolean isPassing = false;
+        BattleshipUtils.ShipSpecs shipSpecs = BattleshipUtils.ShipSpecs.SUBMARINE;
+        
+        switch (shipSpecs) {
+            case AIRCRAFT_CARRIER:
+                //We don't want to do anything here.
+                break;
+            case BATTLESHIP:
+                //We don't want to do anything here.
+                break;
+            case SUBMARINE:
+                isPassing = true;
+                break;
+            case DESTROYER:
+                //We don't want to do anything here.
+                break;
+            case PATROL_BOAT:
+                //We don't want to do anything here.
+                break;
+            default:
+                //We don't want to do anything here.
+        }
+        
+        System.out.println("testShipSpecsLong = pass " + isPassing);
+    }
+
+    public static void testShipSpecsShort() {
+        boolean isPassing = false;
+        BattleshipUtils.ShipSpecs shipSpecs = BattleshipUtils.ShipSpecs.PATROL_BOAT;
+        
+        switch (shipSpecs) {
+            case PATROL_BOAT:
+                isPassing = true;
+                break;
+            default:
+                //We don't want to do anything here.
+        }
+        
+        System.out.println("testShipSpecsShort pass = " + isPassing);
+    }
+
+    public static void printBoardSpecs() {
+        for (BattleshipUtils.BoardSpecs specs : BattleshipUtils.BoardSpecs.values()) {
+            System.out.println(specs + " is a " + specs.rows + "X" + specs.cols + " grid with " + specs.numShips + " ships.");
+        }        
+    }
+
     public static void main(String[] args) {
-        TestBattleshipUtils.testShipTypes();
+        TestBattleshipUtils.printShipSpecs();
+        System.out.println("\n");
+        TestBattleshipUtils.testShipSpecsLong();
+        System.out.println("\n");
+        TestBattleshipUtils.testShipSpecsShort();
+        System.out.println("\n");
+        TestBattleshipUtils.printBoardSpecs();
     }
 }
