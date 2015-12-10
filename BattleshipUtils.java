202ca37767f1c654335811fca2957546f160cbc0
mrsbrock <docs.jennifbrock@gmail.com>
2015-12-10 11:18:59 -0600

    Added utility class

---
+++ BattleshipUtils.java
@@ -0,0 +1,10 @@
+/*
+ *BattleshipUtils is a helper/utility class for the Battleship game.
+ */
+public class BattleshipUtils {
+    
+    // These are varaibles that are used to display the board.
+    public static char DISPLAY_HIT = 'X';
+    public static char DISPLAY_MISS = '_';
+    public static char DISPLAY_UNGUESSED = 'O';
+}
